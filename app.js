59a424bde8f4356051be34a457f755b38343ae1d
meashishpokhrel <meashishpokhrel@gmail.com>
2020-01-18 10:21:50 +0545

    Reading INput form HTML

--- app.js
+++ app.js
@@ -7,16 +7,36 @@
     //UI CONTROLLER
  var UIController = (function(){
 
-
+        var DOMstrings = {
+            inputType: '.add__type',
+            inputDesc: '.add__description',
+            inputValue: '.add__value',
+            inputBtn: '.add__btn'
+        };
+    return{
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value, //will be eithre inc or exp
+                description: document.querySelector(DOMstrings.inputDesc).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+            
+        }, 
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
  })();
 
     //GLOABAL APP CONTROLLER
  var controller = (function(budgetctrl,UIctrl){
-  
+    
+    var DOM = UIctrl.getDOMstrings();
     var ctrlAddItem = function (){
 
         // Get input data
-
+            var input = UIctrl.getInput();
+            console.log(input);
         //Add the item to the budget controller
 
         //Add the new item to the UI
@@ -24,11 +44,11 @@
         //Calculate the budget
 
         //Display the budget on the UI
-        console.log('working');
+         
     }
 
 
-    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
 
     document.addEventListener('keypress',function(event){
         if (event.keycode === 13 || event.which === 13){
